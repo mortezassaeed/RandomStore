cbb90b0c543c48bf6285b9017c1d8c0b22fb26ae
mortezassaeed@gmail.com <mortezassaeed@gmail.com>
2023-01-15 23:25:49 +0330

    enriched GroceryStore project with data generator

--- GroceryStore/Entity.cs
+++ GroceryStore/Entity.cs
@@ -1,10 +1,64 @@
-﻿namespace GroceryStore;
+﻿using System.CodeDom.Compiler;
 
-internal class Entity
+namespace GroceryStore;
+
+internal class Product
 {
-    public Guid EntityId { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public required Guid EntityId { get; set; }
+    public required string Name { get; set; }
     public string Description { get; set; } = string.Empty;
     public double Price { get; set; }
     public DateTime FetchDate { get; set; }
-} 
+}
+
+
+internal class ProductService
+{
+    private List<Product> _products { get; set; }
+    private readonly IReadOnlyList<DateTime> _allowedDate;
+    private readonly IReadOnlyList<double> _allowedPrice;
+    private Random _random;
+
+    public ProductService()
+    {
+        _random = new Random();
+        _products = new List<Product>();
+        _allowedDate = new List<DateTime>() { DateTime.Now, DateTime.Now.AddDays(-1) }.AsReadOnly();
+        _allowedPrice = new List<double>() { 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000 };
+    }
+
+
+    public ProductService GenerateRandomProducts(int collectionCount)
+    {
+        for (int i = 1; i <= collectionCount; i++)
+        {
+            var product = GetRandomProductData();
+            _products.Add(product);
+            // store in radis or something else ??
+        }
+        return this;
+    }
+
+    public IEnumerable<Product> GetProducts() =>
+        _products;
+    
+    private Product GetRandomProductData()
+    {
+        int randomProductNum = 0;
+        DateTime randomDate;
+        double randomPrice;
+        do
+        {
+            randomProductNum = _random.Next(1, 10000);
+            var randomDateNum = _random.Next(_allowedDate.Count);
+            randomDate = _allowedDate[randomDateNum];
+
+            var randomPriceNum = _random.Next(_allowedPrice.Count);
+            randomPrice = _allowedPrice[randomPriceNum];
+
+        } while (_products.Any(p => p.Name == $"Product_{randomProductNum}" && p.FetchDate.Day == randomDate.Day));
+
+        return new Product { EntityId = Guid.NewGuid(), Name = $"Product_{randomProductNum}", FetchDate = randomDate, Description = "", Price = randomPrice };
+    }
+}
+
--- GroceryStore/Program.cs
+++ GroceryStore/Program.cs
@@ -1,36 +1,48 @@
 ﻿
 using GroceryStore;
-using System.Security.Cryptography;
 
-List<Entity> productList = new List<Entity>();
-
+List<Product> productList = new ProductService().GenerateRandomProducts(10000).GetProducts().ToList();
 
 var gpByNameAndPrice = productList
-    .Where(p => p.FetchDate <= DateTime.Now && p.FetchDate >= DateTime.Now.AddDays(-1))
-    .GroupBy(e => new { e.Name, e.Price }).Select(g => new { g.Key.Name, g.Key.Price, ProductCount = g.Count() });
+    //.Where(p => p.FetchDate <= DateTime.Now && p.FetchDate >= DateTime.Now.AddDays(-1))
+    .GroupBy(e => new { e.Name, e.Price }).Select(g => new { g.Key.Name, g.Key.Price, ProductCount = g.Count() }).ToList();
 
 
-var withoutChangeProducts = gpByNameAndPrice.Where(p => p.ProductCount == 2);
+var withoutChangeProducts = gpByNameAndPrice.Where(p => p.ProductCount == 2).ToList();
 
-var gpByNameAfterGpByNameAndPrice = gpByNameAndPrice.GroupBy(p => new { p.Name }).Select(g => new { g.Key.Name, ProductCount = g.Count() });
+var gpByNameAfterGpByNameAndPrice = gpByNameAndPrice.GroupBy(p => new { p.Name }).Select(g => new { g.Key.Name, ProductCount = g.Count() }).ToList();
 
 
 //(1)
-var changedPrice = gpByNameAfterGpByNameAndPrice.Where(p => p.ProductCount == 2);
+var changedPrice = gpByNameAfterGpByNameAndPrice.Where(p => p.ProductCount == 2).ToList();
 
-var newOrRemovedProduct = gpByNameAfterGpByNameAndPrice.Where(p => p.ProductCount == 1);
+var newOrRemovedProduct = gpByNameAfterGpByNameAndPrice.Where(p => p.ProductCount == 1).ToList();
 
 //(2)
-var removedProduct = productList.Where(p => newOrRemovedProduct.Select(nr => nr.Name).Contains(p.Name) && p.FetchDate.Day == DateTime.Now.AddDays(-1).Day); //TODO : not perfect. needed more effort
+var removedProduct = productList.Where(p => newOrRemovedProduct.Select(nr => nr.Name).Contains(p.Name) && p.FetchDate.Day == DateTime.Now.AddDays(-1).Day).ToList(); //TODO : not perfect. needed more effort
 
 //(3)
-var newProduct = productList.Where(p => newOrRemovedProduct.Select(nr => nr.Name).Contains(p.Name) && p.FetchDate.Day == DateTime.Now.Day); //TODO : not perfect. needed more effort
+var newProduct = productList.Where(p => newOrRemovedProduct.Select(nr => nr.Name).Contains(p.Name) && p.FetchDate.Day == DateTime.Now.Day).ToList(); //TODO : not perfect. needed more effort
 
+
+Console.ReadLine();
 
 
 /*
- 
-   A = p1 200 2 
+    productname - price - count        
+
+
+   first group by posible data 
+        p1 - 200 - 2
+        p2 - 300 - 1
+        p2 - 400 - 1
+
+
+    after second group by 
+
+   A = p2 - [] - 2 
+    
+
         => without change
    A => p2 100 1
         p2 200 1
@@ -39,11 +51,4 @@ var newProduct = productList.Where(p => newOrRemovedProduct.Select(nr => nr.Name
         => removed
    A => p4 500 (for today)
         => new
-
  */
-
-
-
-
-
-
